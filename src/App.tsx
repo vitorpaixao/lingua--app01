adeaf54d8fa57913ed41fbc604d5b629b709aae3
vitorpaixao <vitor@vitorpaixao.com>
2026-05-12 14:45:49 +0000

    Update from Lingua 2026-05-12 14:45

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,16 @@ function App() {
 
   return (
     <div className="app">
-        <h1>Vitor PX</h1>
+        <h1>Vetta Design team</h1>
       <p>
         Edit <code>src/App.tsx</code> and the page reloads.
       </p>
       <div className="counter">
         <button onClick={() => setCount(count + 1)}>count is {count}</button>
       </div>
+      <button style={{ backgroundColor: 'green', color: 'white', padding: '10px 20px', border: 'none', borderRadius: '4px', cursor: 'pointer' }}>
+        Vitor
+      </button>
     </div>
   )
 }
